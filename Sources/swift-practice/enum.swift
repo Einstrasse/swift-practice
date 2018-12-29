04626c095849144f212e49287088cf7168d86b4e
Einstrasse <hg9587@naver.com>
2018-12-29 14:44:20 +0900

    enum.swift solved

--- Sources/swift-practice/enum.swift
+++ Sources/swift-practice/enum.swift
@@ -2,13 +2,19 @@ class Enum {
 	enum NetworkError {
 		case invalidParameter(String, String)
 		case timeout
+		var message: String {
+			switch self {
+				case .invalidParameter(let field, let message): return field + " " + message
+				case .timeout: return "타임아웃입니다."
+			}
+		}
 	}
 
-	let temp: NetworkError = .invalidParameter("email", "이메일 형식이 올바르지 않습니다.")
+	let error: NetworkError = .invalidParameter("email", "이메일 형식이 올바르지 않습니다.")
 
 
 	func go() {
-		switch temp {
+		switch error {
 			case .invalidParameter(let field, let message) :
 				print (field)
 				print (message)
@@ -16,5 +22,6 @@ class Enum {
 			default:
 			break
 		}
+		print (error.message)
 	}
 }
